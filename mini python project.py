17373d28b634663585c376cfd37cd5e9c960aa4f
AdityaDellikar <119943546+AdityaDellikar@users.noreply.github.com>
2022-12-06 18:20:23 +0530

    Add files via upload

---
+++ mini python project.py
@@ -0,0 +1,30 @@
+hh=int(input("hour:"))
+mm=int(input("minute:"))
+def findAngle(hh, mm):
+ 
+    # handle 24-hour notation
+    hh = hh % 12
+ 
+    # find the position of the hour's hand
+    h = (hh * 360) // 12 + (mm * 360) // (12 * 60)
+ 
+    # find the position of the minute's hand
+    m = (mm * 360) // (60)
+ 
+    # calculate the angle difference
+    angle = abs(h - m)
+ 
+    # consider the shorter angle and return it
+    if angle > 180:
+        angle = 360 - angle
+ 
+    return angle
+ 
+ 
+# Clock Angle Problem
+if __name__ == '__main__':
+ 
+
+ 
+    print("output:",findAngle(hh, mm))
+
